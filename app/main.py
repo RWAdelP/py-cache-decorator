6ecbcf480a81f6ebcd50d1cf7db4b239cd5fc1d2
Adel Pereira <nic.gis.office@gmail.com>
2026-04-13 15:07:45 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,19 +1,19 @@
 from typing import Callable
 import functools
 
-g_results = {}
+cache_store = {}
 
 
 def cache(func: Callable) -> Callable:
     @functools.wraps(func)
-    def wrapper(*args,**kwargs) -> Callable:
-        key = (func, args, tuple(sorted(kwargs.items()))) 
-        if key in g_results:
+    def wrapper(*args, **kwargs) -> Callable:
+        key = (func, args, tuple(sorted(kwargs.items())))
+        if key in cache_store:
             print("Getting from cache")
-            result = g_results.get(key)
+            result = cache_store.get(key)
         else:
             print("Calculating new result")
-            result = func(*args,**kwargs)
-            g_results[key] = result            
+            result = func(*args, **kwargs)
+            cache_store[key] = result
         return result
     return wrapper
