d4c23be050cb9e0e61fa97a7fa913ca0d1066180
Adel Pereira <nic.gis.office@gmail.com>
2026-04-13 14:53:12 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
 from typing import Callable
+import functools
+
+g_results = {}
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    @functools.wraps(func)
+    def wrapper(*args) -> Callable:
+        result = g_results.get((func.__name__, args))
+        if result is None:
+            result = func(*args)
+            g_results[(func.__name__, args)] = result
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+        return result
+    return wrapper
